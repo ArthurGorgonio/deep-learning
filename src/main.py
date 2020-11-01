0db1748618107cd7375d91519d1a133ad2f02341
ArthurGorgonio <gorgonioarthur@gmail.com>
2020-11-01 14:23:36 -0300

    Add Sigmoid Neuron

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 import perceptron
+from sigmoid import Sigmoid
 
 print('Solve Logical Ports using Perceptron!!')
 print('\nAnd:\n')
@@ -10,3 +11,7 @@ logical_and_solv = np.array([0, 0, 0, 1]).T
 perceptron_and = perceptron.Perceptron(0.01, logical_and_data,
                                        logical_and_solv, 'step')
 perceptron_and.run()
+
+print('Using a sigmoid neuron')
+sig = Sigmoid(0.01, logical_and_data, logical_and_solv, 'sigmoid')
+sig.run()
--- src/utils.py
+++ src/utils.py
@@ -12,6 +12,9 @@ class Activation:
     def __step(self, y_pred: float) -> int:
         return np.where(y_pred > 0, 1, 0)
 
+    def __sigmoid(self, x: float) -> float:
+        return 1.0 / (1.0 + np.exp(-x))
+
 
 # TODO Define a singleton pattern here to recovery the same instance when use
 #   more than one activation function and/or have more than one object that
