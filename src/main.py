0b04e9b74f3b9a80b20bc0241ae74a600a776e42
ArthurGorgonio <gorgonioarthur@gmail.com>
2020-11-02 00:18:31 -0300

    Testing new main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
 import numpy as np
 
-import perceptron
+from perceptron import Perceptron
 from sigmoid import Sigmoid
 
 print('Solve Logical Ports using Perceptron!!')
-print('\nAnd:\n')
 logical_and_data = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 logical_and_solv = np.array([0, 0, 0, 1]).T
 
-perceptron_and = perceptron.Perceptron(0.01, logical_and_data,
-                                       logical_and_solv, 'step')
-perceptron_and.run()
-
+perceptron_and = Perceptron(0.01, logical_and_data, logical_and_solv, 'step')
+perceptron_and.run(101, 10)
+#
 print('Using a sigmoid neuron')
 sig = Sigmoid(0.01, logical_and_data, logical_and_solv, 'sigmoid')
-sig.run()
+sig.run(1001, 100)
